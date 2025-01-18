dfd1c66133f788d0491a35b4e0c861cef3bf519e
VishnuPR <vishnu@webtreeonline.com>
2025-01-18 16:06:02 +0530

    first commit

--- src/main/java/com/doker/demo/controller/DockerController.java
+++ src/main/java/com/doker/demo/controller/DockerController.java
@@ -1,13 +1,19 @@
 package com.doker.demo.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class DockerController {
 
+    @Value("${application.demo}")
+    private String demo;
+
     @GetMapping("/docker-demo")
-    public String dockerDemo(){
-        return "<H1>DOCKER DEMO</H1>";
+    public String dockerDemo() {
+        return "<H1>" + demo + "</H1>";
     }
 }
